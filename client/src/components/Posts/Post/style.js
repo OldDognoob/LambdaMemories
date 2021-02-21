415f368daba92b42eb8dbc93ba2d411a044fa7bc
Dimos-Christidis <christidis.dimosthenis@gmail.com>
2021-02-21 20:05:00 +0200

    wip

---
+++ client/src/components/Posts/Post/style.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import { Card, CardActions, CardContent, CardMedia, Button, Typography } from '@material-ui/core/';
+import moment from 'moment';
+
+import { useDispatch } from 'react-redux';
+
+import { likePost, deletePost } from '../../../actions/posts';
+import useStyles from './styles';
+
+const Post = () => {
+    return(
+        <div>Starting Soon</div>
+    )
+}
+export default Post;
