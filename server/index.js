9e0b2dfe3a2286640c93efc8e28e71eb867caea3
Dimos-Christidis <christidis.dimosthenis@gmail.com>
2021-02-21 17:52:24 +0200

    changes

--- server/index.js
+++ server/index.js
@@ -10,6 +10,7 @@ app.use(bodyParser.json({limit:"30mb", extended:true}));
 app.use(bodyParser.urlencoded({limit:"30mb", extended:true}));
 app.use(cors());
 
+
 const CONNECTION_URL = 'mongodb+srv://lambdamemories:lambdamemories7783@cluster0.oqdtx.mongodb.net/myFirstDatabase?retryWrites=true&w=majority';
 const PORT = process.env.PORT || 5000;
 
